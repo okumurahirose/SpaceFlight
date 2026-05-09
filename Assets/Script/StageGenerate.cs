96d342aa410751e2914af63a97533326b57b68a7
Hirose2228 <hirose2228@gmail.com>
2026-05-09 15:31:13 +0900

    add : 自動でステージを生成するStageGeneraterを実装しました。
    スクリプトStageGenerateを追加し、ステージが自動生成されるようにしました。

--- Assets/Script/StageGenerate.cs
+++ Assets/Script/StageGenerate.cs
@@ -4,9 +4,10 @@ using UnityEngine;
 public class StageGenerate : MonoBehaviour
 {
     [SerializeField] private GameObject Character;
-    [SerializeField] private GameObject[] Stages;
+    [SerializeField] private GameObject[] Stages; //生成するStageプレハブの配列
+    [SerializeField] private GameObject Parent; //生成したStageのクローンをまとめるための親オブジェクト
 
-    List<GameObject> GeneratedStages = new List<GameObject>();
+    [SerializeField] private List<GameObject> GeneratedStages = new List<GameObject>();
 
     float StageSize = 30;
     float GeneratePosition = 90;
@@ -15,20 +16,41 @@ public class StageGenerate : MonoBehaviour
     void Start()
     {
         CharacterPosition = Character.transform.position.z;
+        
     }
 
     
     void Update()
-    {
+    {   
+        CharacterPosition = Character.transform.position.z;
+
         if(GeneratedStages.Count <= 6)
         {
-            GameObject target = Stages[Random.Range(0,Stages.Length)];
-            GameObject Instance = Instantiate(
-                                        target,
-                                        new (0.0f,0.0f,GeneratePosition),
-                                        Quaternion.identity);
-            GeneratedStages.Add(Instance);
-            GeneratePosition += StageSize;
+            GenerateStage();
         }
+        else if(CharacterPosition > GeneratedStages[2].transform.position.z)
+        {
+            DestroyStage();
+        }
+
+    }
+    void GenerateStage()
+    {
+        int target = Random.Range(0,Stages.Length);
+
+        GameObject Instance = Instantiate(
+                                    Stages[target],
+                                    new Vector3(0.0f,0.0f,GeneratePosition),
+                                    Quaternion.identity);
+        GeneratedStages.Add(Instance);
+        GeneratePosition += StageSize;
+        Instance.transform.SetParent(Parent.transform);
+    }
+
+    void DestroyStage()
+    {
+        GameObject target = GeneratedStages[0];
+        GeneratedStages.RemoveAt(0);
+        Destroy(target);
     }
 }
