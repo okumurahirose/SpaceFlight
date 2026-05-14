b349facb8e95557236591709be8b69e55ca9e826
Hirose2228 <hirose2228@gmail.com>
2026-05-14 11:56:53 +0900

    add : 機体が障害物に衝突した時におこるイベントを追加しました
    機体が障害物に衝突すると全ての移動を停止して、アニメーションが再生されるようにしました。
    機体が揺れながら一回転するようなアニメーションです。
    
    problem ： アニメーションを追加したことによってFlightControllスクリプトのSetRotate()とReturnRotate()が機能しなくなりました。
                アニメーションがtransform.rotationをプロパティとしているためスクリプトのz軸回転が上書きされたのだと思います
    
    next : 上記の問題の修正

--- Assets/Script/StageGenerate.cs
+++ Assets/Script/StageGenerate.cs
@@ -7,27 +7,32 @@ public class StageGenerate : MonoBehaviour
     [SerializeField] private GameObject[] Stages; //生成するStageプレハブの配列
     [SerializeField] private GameObject Parent; //生成したStageのクローンをまとめるための親オブジェクト
 
-    [SerializeField] private List<GameObject> GeneratedStages = new List<GameObject>();
+    [SerializeField] private List<GameObject> GeneratedStages = new List<GameObject>(); //生成したステージをまとめるリスト
 
-    float StageSize = 30;
-    float GeneratePosition = 90;
-    float CharacterPosition;
+    float StageSize = 30; //ステージのZ軸幅 [m]
+    float GeneratePosition; //ステージの生成位置 [m]
+    float CharacterPosition; //機体の位置 [m]
 
     void Start()
     {
+        //機体の位置情報の初期化
         CharacterPosition = Character.transform.position.z;
-        
+        //初めに置かれているステージの数に合わせて次に生成するステージ位置の初期化
+        GeneratePosition = StageSize * (GeneratedStages.Count + 1) ;
     }
 
     
     void Update()
     {   
+        //機体の位置の更新
         CharacterPosition = Character.transform.position.z;
 
-        if(GeneratedStages.Count <= 6)
+        //現在生成されたステージ数が６未満だったらステージを作る
+        if(GeneratedStages.Count < 6)
         {
             GenerateStage();
         }
+        //ステージが６つで機体の位置が特定の位置を超えたら古いステージを消去する
         else if(CharacterPosition > GeneratedStages[2].transform.position.z)
         {
             DestroyStage();
@@ -36,19 +41,24 @@ public class StageGenerate : MonoBehaviour
     }
     void GenerateStage()
     {
+        //生成するステージをランダムで取得
         int target = Random.Range(0,Stages.Length);
 
+        //生成
         GameObject Instance = Instantiate(
                                     Stages[target],
                                     new Vector3(0.0f,0.0f,GeneratePosition),
                                     Quaternion.identity);
+
+        //生成したステージをリストと親オブジェクトに加え、次の生成位置を更新
         GeneratedStages.Add(Instance);
-        GeneratePosition += StageSize;
         Instance.transform.SetParent(Parent.transform);
+        GeneratePosition += StageSize;
     }
 
     void DestroyStage()
-    {
+    {   
+        //リストから最も古いステージを取得して消去
         GameObject target = GeneratedStages[0];
         GeneratedStages.RemoveAt(0);
         Destroy(target);
