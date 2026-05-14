4dd7cf8cb4e1ceb5fe94bb5871425e7252b28d5a
Hirose2228 <hirose2228@gmail.com>
2026-05-14 11:56:53 +0900

    add : 機体が障害物に衝突した時におこるイベントを追加しました
    機体が障害物に衝突すると全ての移動を停止して、アニメーションが再生されるようにしました。
    機体が揺れながら一回転するようなアニメーションです。
    
    problem ： アニメーションを追加したことによってFlightControllスクリプトのSetRotate()とReturnRotate()が機能しなくなりました。
                アニメーションがtransform.rotationをプロパティとしているためスクリプトのz軸回転が上書きされたのだと思います
    
    next : 上記の問題の修正

--- Assets/Script/FlightControll.cs
+++ Assets/Script/FlightControll.cs
@@ -20,39 +20,54 @@ public class FlightControll : MonoBehaviour
 
     Vector3 MoveDirection = Vector3.zero;
     InputAction _moveAction;
+    Animator _animator;
+
+    const float StunTime = 0.5f; //Stun状態の長さ
+    float RecoverTime = 0.0f; //残りのStun状態時間
+    
 
     void Start()
     {
         _moveAction = InputSystem.actions.FindAction("Move");
+        _animator = GetComponent<Animator>();
         AcceleratedSpeed = 0.0f;
     }
 
 
     void Update()
     {
-        //自動で前に進む
-        AcceleratedSpeed += Acceleration_Z * Time.deltaTime;
-        MoveDirection.z = Mathf.Clamp(AcceleratedSpeed,0,Speed_Z) * Time.deltaTime;
-
-        //横方向への移動
-        MoveDirection.x = _moveAction.ReadValue<Vector2>().x * Speed_X * Time.deltaTime;
-        //縦方向への移動
-        MoveDirection.y = _moveAction.ReadValue<Vector2>().y * Speed_Y * Time.deltaTime;
-
-        //機体をZ軸回転させる（q:左　e:右）
-        if(Keyboard.current.qKey.isPressed){
-            float abs = 1;
-            SetRotate(abs);
-        }
-        else if(Keyboard.current.eKey.isPressed)
+        if (IsStun())
         {
-            float abs = -1;
-            SetRotate(abs);
+            AcceleratedSpeed = 0.0f;
+            MoveDirection = Vector3.zero;
+            RecoverTime -= Time.deltaTime;
         }
-        //rKeyで機体を水平に戻す
-        else if(Keyboard.current.rKey.wasPressedThisFrame)
-        {
-            StartCoroutine(ReturnRotate());
+        else
+        {   
+            //自動で前に進む
+            AcceleratedSpeed += Acceleration_Z * Time.deltaTime;
+            MoveDirection.z = Mathf.Clamp(AcceleratedSpeed,0,Speed_Z) * Time.deltaTime;
+
+            //横方向への移動
+            MoveDirection.x = _moveAction.ReadValue<Vector2>().x * Speed_X * Time.deltaTime;
+            //縦方向への移動
+            MoveDirection.y = _moveAction.ReadValue<Vector2>().y * Speed_Y * Time.deltaTime;
+
+            //機体をZ軸回転させる（q:左　e:右）
+            if(Keyboard.current.qKey.isPressed){
+                float abs = 1;
+                SetRotate(abs);
+            }
+            else if(Keyboard.current.eKey.isPressed)
+            {
+                float abs = -1;
+                SetRotate(abs);
+            }
+            //rKeyで機体を水平に戻す
+            else if(Keyboard.current.rKey.wasPressedThisFrame)
+            {
+                StartCoroutine(ReturnRotate());
+            }
         }
 
         //機体の移動後の位置
@@ -72,6 +87,8 @@ public class FlightControll : MonoBehaviour
         //機体の角度をいくら傾けるか
         float target = abs * RotateRate;
         transform.Rotate(0,0,target);
+        Debug.Log(transform.eulerAngles.z);
+
     }
 
     //機体の角度を自動で水平に戻す
@@ -90,5 +107,22 @@ public class FlightControll : MonoBehaviour
 
         //完全に水平にする
         transform.rotation = Quaternion.Euler(0.0f,0.0f,0.0f);
+    }
+
+    //Stun状態であるか
+    bool IsStun()
+    {
+        return RecoverTime > 0.0f;
+    }
+
+    //Enemyに接触したら衝突イベントを実行
+    void OnCollisionEnter(Collision other)
+    {   
+        Destroy(other.gameObject);
+        if(IsStun()) return;
+        if(other.gameObject.tag == "Enemy"){
+            RecoverTime = StunTime;
+            _animator.SetTrigger("Collision");
+        }
     }
 }
--- Assets/Script/FollowTarget.cs
+++ Assets/Script/FollowTarget.cs
@@ -5,24 +5,24 @@ public class FollowTarget : MonoBehaviour
     [SerializeField] private GameObject TargetObject;
     [SerializeField] private float FollowSpeed; // [m/s]
 
-    float _diff; // [m]
+    Vector3 _diff; // [m]
     void Start()
     {   
         //TargetObjectとの距離を取得
-        _diff = TargetObject.transform.position.z - transform.position.z;
+        _diff = TargetObject.transform.position - transform.position;
     }
 
     //Update()の処理が終わってから追いかける
     void LateUpdate()
     {
         //スムーズに近づく
-        float new_Z = Mathf.Lerp(
-            transform.position.z,
-            TargetObject.transform.position.z - _diff, /* = 目標 */
+     Vector3 pos = Vector3.Lerp(
+            transform.position,
+            TargetObject.transform.position - _diff, /* = 目標 */
             FollowSpeed * Time.deltaTime);
 
-        Vector3 pos = transform.position;
-        pos.z = new_Z;
+        //Vector3 pos = transform.position;
+        //pos.z = new_Z;
 
         //反映
         transform.position = pos;
