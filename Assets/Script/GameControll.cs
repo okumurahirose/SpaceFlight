bb1e23f63285828a75d9fa6efd8853ef5bf2e19b
Hirose2228 <hirose2228@gmail.com>
2026-05-20 10:16:14 +0900

    add : Lifeが全損した場合の状態の追加と強制停止の追加、スコアの追加

--- Assets/Script/GameControll.cs
+++ Assets/Script/GameControll.cs
@@ -1,26 +1,43 @@
 using UnityEditor.IMGUI.Controls;
 using UnityEngine;
 using UnityEngine.UI;
+using TMPro;
 
 public class GameControll : MonoBehaviour
 {
+    [SerializeField] private GameObject Player; 
     [SerializeField] private Image[] Lifes;
+    [SerializeField] private TextMeshProUGUI Score;
     const int LifeCount = 3;
     int NowLifeCount;
+    int position;
 
     void Start()
     {
+        //NowLifeCountの初期化
         NowLifeCount = LifeCount;
+        //Scoreの初期化
+        position = (int)Player.transform.position.z;
+        Score.text = "Score : " + position + "m";
+    }
+
+    void Update()
+    {
+        //Scoreの更新
+        position = (int)Player.transform.position.z;
+        Score.text = "Score : " + position + "m";
     }
 
 
     public void ManageLife()
     {
-        if(NowLifeCount <= 0) return;
-
+        
         Lifes[NowLifeCount-1].gameObject.SetActive(false);
         NowLifeCount--;
 
+        //Lifeが全損したら死亡状態を発信
+        if(NowLifeCount == 0)  Player.SendMessage("Dead");
+        
         Debug.Log(NowLifeCount);
 
         return;
