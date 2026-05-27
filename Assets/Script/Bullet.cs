0507b45e39971a1cd9431607f3801b6e6422f653
Hirose2228 <hirose2228@gmail.com>
2026-05-27 19:04:04 +0900

    fix : BulletをPoolから取り出した時に前回の慣性を引き継いであっちこっちに飛んでしまうのを修正
    Bulletの存在時間が終わった時にPoolに返却すると同時にRigidbodyから慣性を０にするようにしました。

--- Assets/Script/Bullet.cs
+++ Assets/Script/Bullet.cs
@@ -3,28 +3,37 @@ using UnityEngine;
 public class Bullet : MonoBehaviour
 {
     BulletPool Pool;
-    [SerializeField] private float ActiveTime;
-    [SerializeField] private float Speed;
-    float RestActiveTime;
+    [SerializeField] private float ActiveTime; //Bulletの存在可能時間
+    [SerializeField] private float Speed; //Bulletのスピード
+    float RestActiveTime; //残りの存在時間
+    Rigidbody _rigidbody;
 
     void Awake()
     {
+        //BulletPoolを取得
         Pool = FindFirstObjectByType<BulletPool>();
     }
 
     void Start()
     {
         RestActiveTime = ActiveTime;
+        _rigidbody = GetComponent<Rigidbody>();
     }
 
     void Update()
     {
+        //直進処理と時間計算
         transform.Translate(0,Speed * Time.deltaTime,0);
         RestActiveTime -= Time.deltaTime;
-
+        
         if(RestActiveTime < 0)
         {
+            //Poolに返却する
             Pool.Release(this.gameObject);
+
+            //Bulletを初期状態に戻す
+            _rigidbody.linearVelocity = new Vector3();
+            _rigidbody.angularVelocity = new Vector3();
             RestActiveTime = ActiveTime;
             
         }
