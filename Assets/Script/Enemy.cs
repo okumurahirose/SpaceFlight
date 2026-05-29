1ff3e39a1a63b2547d4498812cca0ced864c043c
Hirose2228 <hirose2228@gmail.com>
2026-05-29 11:23:26 +0900

    add : BulletによってEnemyを破壊できるようにしました。
    EnemyプレハブからそれぞれのプレハブにLifeを設定することができ、Lifeの回数Bulletが当たったら破壊できます。

---
+++ Assets/Script/Enemy.cs
@@ -0,0 +1,22 @@
+using System;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+    [SerializeField] private  int DefalutLifeCount;
+    int RestLifeCount;
+
+    void Start()
+    {
+        RestLifeCount = DefalutLifeCount;
+    }
+
+    public void LifeDecrement()
+    {
+        RestLifeCount--;
+        if(RestLifeCount <= 0)
+        {
+            Destroy(this.gameObject);
+        }
+    }
+}
